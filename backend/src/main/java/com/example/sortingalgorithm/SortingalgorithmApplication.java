8f03757ae459ca857ef0a4bbc00ac2802cd14590
nakieb3laa <hany.ayman606@gmail.com>
2026-03-03 23:32:29 +0200

    backend folders done

---
+++ backend/src/main/java/com/example/sortingalgorithm/SortingalgorithmApplication.java
@@ -0,0 +1,13 @@
+package com.example.sortingalgorithm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SortingalgorithmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SortingalgorithmApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/sortingalgorithm/SortingalgorithmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sortingalgorithm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SortingalgorithmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
